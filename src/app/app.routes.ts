67584befd297dd9488654b05f9b27e43a4fe8068
tiziasan <tizianosantilli@gmail.com>
2024-05-07 15:15:11 +0200

    Added Contribution

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {AboutComponent} from "./about/about.component";
 import {HomeComponent} from "./home/home.component";
+import {ContributeComponent} from "./contribute/contribute.component";
 
 export const routes: Routes = [
   { path: 'dashboard-component', component: DashboardComponent },
   { path: 'about-component', component: AboutComponent },
   { path: 'home-component', component: HomeComponent },
+  { path: 'contribute-component', component: ContributeComponent },
+
 ];
