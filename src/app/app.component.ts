d215974332face15cb45a21dda38cb048bb693af
tiziasan <tizianosantilli@gmail.com>
2024-05-07 14:34:35 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButton} from "@angular/material/button";
+<<<<<<< Updated upstream
+=======
+import {AboutComponent} from "./about/about.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-root',
